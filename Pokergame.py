ed2078bc4894c658e49f6ff19a6a55789e868800
Luumo <34240620+Luumo@users.noreply.github.com>
2019-02-27 14:31:18 +0100

    idk what the fuck i did

--- Pokergame.py
+++ Pokergame.py
@@ -6,20 +6,59 @@ import sys
 # import cardlib as pc
 
 
-class TableScene(QGraphicsScene):
-    """ A scene with a table cloth background """
+class MyWindow(QGroupBox):
     def __init__(self):
-        super().__init__()
-        self.tile = QPixmap('cards/table.png')
-        self.setBackgroundBrush(QBrush(self.tile))
+        super().__init__("My window content")  # Call the QWidget initialization as well!
+        car = TableCards()
+        betButton = QPushButton("Bet")
+        foldButton = QPushButton("Fold")
 
-class CardItem(QGraphicsSvgItem):
-    """ A simple overloaded QGraphicsSvgItem that also stores the card position """
-    def __init__(self, renderer, position):
-        super().__init__()
-        self.setSharedRenderer(renderer)
-        self.position = position
+
+        # creates horizontal objects
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(betButton)
+        hbox.addWidget(foldButton)
+
+        # uses the horizontal objects, and aligns them vertically
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+
+        self.setGeometry(500, 300, 1000, 500)
+        self.setWindowTitle('POKER GAME')
+
+
+class TableCards(QGroupBox):
+    def __init__(self):
+        super().__init__("Table Cards")
+
+        Flop1 = QLabel("Card 1")
+        Flop2 = QLabel("Card 2")
+        Flop3 = QLabel("Card 3")
+        Turn = QLabel("Turn")
+        River = QLabel("River")
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(Flop1)
+        hbox.addWidget(Flop2)
+        hbox.addWidget(Flop3)
+        hbox.addWidget(Turn)
+        hbox.addWidget(River)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+        self.setLayout(vbox)
 
 
 app = QApplication(sys.argv)
+win = MyWindow()
+table = TableCards()
+
+win.show()
+table.show()
 app.exec_()
