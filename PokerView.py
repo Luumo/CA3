6304ab04177c070ee15d2f1c72a10d2ef3f0f1eb
Luumo <34240620+Luumo@users.noreply.github.com>
2019-03-03 16:47:52 +0100

    added fold button which works

---
+++ PokerView.py
@@ -0,0 +1,57 @@
+from PyQt5.QtCore import *
+from cardlib import *
+
+class TexasHoldEm(QObject):
+    new_pot = pyqtSignal()
+    winner = pyqtSignal(str,)
+
+    def __init__(self):
+        super().__init__()
+        # init players, should be represented by player class ?
+        self.players = [Player("Janne"), Player("Fia")]
+        # init deck
+        self.deck = StandardDeck()
+        # init flop (3 cards)
+        self.table_cards = [self.deck.pop_card() for _ in range(3)]
+        self.pot = 0
+
+    def active_player(self):
+        return self.players[0]
+
+    def cards_on_table(self):
+        # Which cards are on the table?
+        pass
+
+    def fold(self):
+        # if active player folds, other player wins.
+        if self.active_player() == self.players[0]:
+            self.winner.emit(self.players[1].name + " won!")
+        elif self.active_player() == self.players[1]:
+            self.winner.emit(self.players[0].name + " won!")
+
+
+    def call(self):
+        # call previous players decision
+        pass
+
+    def bet(self, amount):
+        self.active_player().bet(amount)
+        self.pot += amount
+        self.new_pot.emit()
+
+
+class Player:
+    new_credits = pyqtSignal()
+
+    def __init__(self, name: str):
+        self.name = name
+        self.credits = 1000
+        self.hand = Hand()
+
+    def active(self):
+        return self.credits > 0
+
+    def bet(self, amount):
+        self.credits -= amount
+        self.new_credits.emit()
+
--- Pokergame.py
+++ Pokergame.py
@@ -2,8 +2,9 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtSvg import *
 from PyQt5.QtWidgets import *
+from PokerView import *
 import sys
-from cardlib import *
+
 
 
 class MainWindow(QGroupBox):
@@ -11,7 +12,7 @@ class MainWindow(QGroupBox):
         super().__init__("Main window")  # Call the QWidget initialization as well!
 
         # creates widgets
-        cv = ControlView(game.pot)
+        cv = ControlView(game)
         pv1 = PlayerView(game.players[0])
         pv2 = PlayerView(game.players[1])
         tv = TableView()
@@ -34,22 +35,25 @@ class MainWindow(QGroupBox):
 
         # Model
         self.game = game
-        self.update_pot()
+        game.winner.connect(self.alert_winner)
 
+    def alert_winner(self, text):
+        msg = QMessageBox()
+        msg.setText(text)
+        msg.exec()
 
-    def update_pot(self):
-        pass
 
 class ControlView(QGroupBox):
-    def __init__(self, pot):
-        super().__init__("Control View")  # Call the QWidget initialization as well!
-        self.pot = pot
+    def __init__(self, game):
+        super().__init__("Control View")
+        self.pot = game.pot
         #Create buttons
         betButton = QPushButton("Bet")
+        betAmmount = QLineEdit()
         foldButton = QPushButton("Fold")
+        foldButton.clicked.connect(game.fold)
         raiseButton = QPushButton("Raise")
         checkButton = QPushButton("Check")
-        betAmmount = QLineEdit()
         self.potLabel = QLabel()
 
         # arrange widgets vertically
@@ -76,24 +80,29 @@ class ControlView(QGroupBox):
             self.potLabel.setText("Pot: ${}".format(self.pot))
 
 
+
 class PlayerView(QGroupBox):
     def __init__(self, player):
-        super().__init__()
+        super().__init__("{}".format(player.name))
         self.player = player
+
         # widgets
         self.valueLabel = QLabel()
         cardView = QLabel("Two Cards")
+
         # arrange vertically
         vbox = QVBoxLayout()
         vbox.addStretch(1)
         vbox.addWidget(self.valueLabel)
         vbox.addWidget(cardView)
+
         # arrange horizontally
         hbox = QHBoxLayout()
         hbox.addStretch(1)
         hbox.addLayout(vbox)
 
         self.setLayout(hbox)
+
         # Model
         self.update_credits()
 
@@ -106,72 +115,18 @@ class TableView(QGroupBox):
         super().__init__("Table View")
         # widgets
         cardLabels = QLabel("Flop, River, Turn")
+
         # arrange horizontally
         hbox = QHBoxLayout()
         hbox.addStretch(1)
         hbox.addWidget(cardLabels)
+
         # arrange vertically
         vbox = QVBoxLayout()
         vbox.addStretch(1)
         vbox.addLayout(hbox)
 
         self.setLayout(vbox)
-
-
-class TexasHoldEm(QObject):
-    new_pot = pyqtSignal()
-
-    def __init__(self):
-        super().__init__()
-        # init players, should be represented by player class ?
-        self.players = [Player("Janne"), Player("Fia")]
-        # init deck
-        self.deck = StandardDeck()
-        # init flop (3 cards)
-        self.table_cards = [self.deck.pop_card() for _ in range(3)]
-        self.pot = 0
-
-    def active_player(self):
-        return self.players[0]
-
-    def cards_on_table(self):
-        # Which cards are on the table?
-        pass
-
-    def the_pot(self):
-        pass
-
-    def fold(self):
-        # if active player press fold, then other player win!
-        pass
-
-    def call(self):
-        # call previous players decision
-        pass
-
-    def bet(self, amount):
-        self.active_player().bet(amount)
-        self.pot += amount
-        self.new_pot.emit()
-
-
-class Player:
-    new_credits = pyqtSignal()
-
-    def __init__(self, name: str):
-        self.name = name
-        self.credits = 1000
-        self.hand = Hand()
-
-    def active(self):
-        return self.credits > 0
-
-    def bet(self, amount):
-        self.credits -= amount
-        self.new_credits.emit()
-
-
-
 
 app = QApplication(sys.argv)
 win = MainWindow(TexasHoldEm())
